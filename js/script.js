0478f6b00a41f2d72cc65813c8af6116b886f446
Buddhadev-coder <sahoobuddhadev696@gmail.com>
2025-10-29 20:18:09 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,75 @@
+const menuBtn = document.getElementById('menuBtn');
+const menu = document.getElementById('menu');
+
+menuBtn.addEventListener('click', () => {
+    menuBtn.classList.toggle('active');
+    menu.classList.toggle('-translate-x-full');
+});
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    speed: 1000,
+    lazy: {
+        loadPrevNext: true,
+    },
+    preloadImages: false,
+    watchSlidesProgress: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 0,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 0,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+    },
+});
+var swiper = new Swiper(".mySwiper2", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    speed: 1000,
+    lazy: {
+        loadPrevNext: true,
+    },
+    preloadImages: false,
+    watchSlidesProgress: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 0,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 0,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+    },
+});
+
+const addClass = document.querySelector('header');
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 800) {
+        addClass.classList.add('color');
+    } else {
+        addClass.classList.remove("color");
+    }
+});
